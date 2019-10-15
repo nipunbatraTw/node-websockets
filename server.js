f01d52569275368ff00f416f560e28c47a8a314e
nipunbatra1804 <nipun.batra@thoughtworks.com>
2019-10-15 12:37:36 +0800

    increase websocket broadcast interval

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const SocketServer = require('ws').Server;
-const path = require('path');
+const express = require("express");
+const SocketServer = require("ws").Server;
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+const INDEX = path.join(__dirname, "index.html");
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .use((req, res) => res.sendFile(INDEX))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new SocketServer({ server });
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+wss.on("connection", ws => {
+  console.log("Client connected");
+  ws.on("close", () => console.log("Client disconnected"));
 });
 
 setInterval(() => {
-  wss.clients.forEach((client) => {
+  wss.clients.forEach(client => {
     client.send(new Date().toTimeString());
   });
-}, 1000);
+}, 10000);
