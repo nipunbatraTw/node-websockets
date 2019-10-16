4f39df5ee0629185dc35c6fd1f6c07b48bf4a243
nipunbatra1804 <nipun.batra@thoughtworks.com>
2019-10-16 17:03:32 +0800

    manual revert of  server.js

--- server.js
+++ server.js
@@ -6,28 +6,23 @@ const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, "index.html");
-const app = express();
 
-let wss = {};
-app
+const server = express()
   .use((req, res) => res.sendFile(INDEX))
   .get("/", () => {
     console.log("received GET request");
   })
-  .post("/", () => {
-    console.log("received post");
-    wss.clients.forEach(client => {
-      console.log("message sent");
-      console.log(typeof client);
-      client.send(new Date().toTimeString());
-    });
-  });
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-wss = new SocketServer({ server: app });
+const wss = new SocketServer({ server });
 
 wss.on("connection", ws => {
   console.log("Client connected");
   ws.on("close", () => console.log("Client disconnected"));
 });
+
+setInterval(() => {
+  wss.clients.forEach(client => {
+    client.send(new Date().toTimeString());
+  });
+}, 10000);
