8b506315817f3fdbab3da2bc01f28271b5219f28
nipunbatra1804 <nipun.batra@thoughtworks.com>
2019-10-16 16:41:03 +0800

    add post endpoint

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const path = require("path");
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, "index.html");
 
+const wss = new SocketServer({ server });
+
 const server = express()
   .use((req, res) => res.sendFile(INDEX))
   .get("/", () => {
@@ -14,12 +16,11 @@ const server = express()
   })
   .post("/", () => {
     wss.clients.forEach(client => {
+      console.log("message sent");
       client.send(new Date().toTimeString());
     });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const wss = new SocketServer({ server });
 
 wss.on("connection", ws => {
   console.log("Client connected");
